d74ee6ac325c8b5c98af4506b47ec10c7837f866
Pat <pat@patsullivan.org>
2025-12-16 03:41:37 +0000

    fix(video-assets): Resolve issue with copy to dist folder during build task

--- devenv.nix
+++ devenv.nix
@@ -12,22 +12,4 @@
   # https://devenv.sh/basics/
   env.DEFAULT_LOCALE = "en_US";
   env.DEFAULT_TIMEZONE = "America/New_York";
-
-  # https://devenv.sh/git-hooks/
-  git-hooks = {
-    enable = true;
-    hooks = {
-      yamllint = {
-        enable = true;
-        settings = {
-          configPath = "${config.env.DEVENV_ROOT}/node_modules/@storm-software/linting-tools/yamllint/config.yml";
-        };
-        files = "\\.(yaml|yml)$";
-        excludes = [
-          "pnpm-lock.yaml"
-          ".pre-commit-config.yaml"
-        ];
-      };
-    };
-  };
 }
