d5a0b0c39839a0dbb9f9805187c8f3e13cfd5869
Pat <pat@patsullivan.org>
2025-12-16 02:56:46 +0000

    chore(monorepo): Update devenv and node modules

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,33 @@
-{ ... }:
+{ config, ... }:
 {
   name = "storm-software/media-kit";
 
   dotenv.enable = true;
-  dotenv.filename = [".env" ".env.local"];
+  dotenv.filename = [
+    ".env"
+    ".env.local"
+  ];
   dotenv.disableHint = true;
 
   # https://devenv.sh/basics/
   env.DEFAULT_LOCALE = "en_US";
   env.DEFAULT_TIMEZONE = "America/New_York";
 
-  # https://devenv.sh/packages/
-  packages = [];
+  # https://devenv.sh/git-hooks/
+  git-hooks = {
+    enable = true;
+    hooks = {
+      yamllint = {
+        enable = true;
+        settings = {
+          configPath = "${config.env.DEVENV_ROOT}/node_modules/@storm-software/linting-tools/yamllint/config.yml";
+        };
+        files = "\\.(yaml|yml)$";
+        excludes = [
+          "pnpm-lock.yaml"
+          ".pre-commit-config.yaml"
+        ];
+      };
+    };
+  };
 }
-
